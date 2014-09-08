d78fdafb6ad51fe97b6211176682cb1263a5b2ee
Baekalfen <mynddal@me.com>
2014-09-08 11:46:39 +0200

    Added file support and postMessages

--- PwettyLogger.py
+++ PwettyLogger.py
@@ -4,10 +4,9 @@
 #  Credit me if you want to...
 #
 
-# TODO: Add file logging
-
 import sys
 import time
+import atexit
 
 class colors:
     OK    = '\033[95m'
@@ -18,43 +17,62 @@ class colors:
     END   = '\033[0m'
 
 logWidth = 80
-logWidth -= 8
-postMsgWidth = 10
+statusMsgWidth = 10
 lineNumbers = True
 preMessage = None#lambda: "\n[ %s%s%s ]\n"%(colors.GREEN,time.strftime('%X %x %Z'),colors.END)
+postMessage = None#lambda: time.strftime('%X %x')
+logFile = None#"log.txt"
+
+logWidth -= 1+len(postMessage()+' ' if postMessage else '')
+if logFile:
+    f = open(logFile, 'a')
 
 def log(colorCode,errorCode,text,task=None):
-    messages = [text[i:i+logWidth-postMsgWidth] for i in range(0, len(text), logWidth-postMsgWidth)]
+    messages = [text[i:i+logWidth-statusMsgWidth] for i in range(0, len(text), logWidth-statusMsgWidth)]
     if str(type(preMessage)).find('function') != -1:
-        sys.stdout.write(preMessage())
+        write(preMessage())
     elif preMessage:
-        sys.stdout.write(preMessage)
+        write(preMessage)
 
     for i,message in enumerate(messages):
-        sys.stdout.write(message.ljust(logWidth-postMsgWidth))
+        write(message.ljust(logWidth-statusMsgWidth))
         if i!=len(messages)-1:
             if lineNumbers:
                 addPostMessage(str(i+1),colors.GREEN)
             else:
-                sys.stdout.write('\n')
-    sys.stdout.flush()
+                write('\n')
 
     if task:
         task()
     addPostMessage(errorCode,colorCode)
 
 def addPostMessage(postMsg,colorCode):
-    postMsg = postMsg[:postMsgWidth-2]
+    postMsg = postMsg[:statusMsgWidth-2]
     if len(postMsg)%2 == 1:
         postMsg+=" "
-    _postMsgWidth = postMsgWidth - len(postMsg)
-    pad = ' '*(_postMsgWidth/2-1)
-    sys.stdout.write("[%s%s%s%s%s]\n"%(pad,colorCode,postMsg,colors.END,pad))
+    _statusMsgWidth = statusMsgWidth - len(postMsg)
+    pad = ' '*(_statusMsgWidth/2-1)
+    write(" [%s%s%s%s%s]"%(pad,colorCode,postMsg,colors.END,pad))
+    if postMessage:
+        write(' %s'%postMessage())
+    write('\n')
+
+def write(text):
+    sys.stdout.write(text)
+    sys.stdout.flush()
+    if logFile:
+        f.write(text)
+
+def closeFile():
+    f.close()
+
+if logFile:
+    atexit.register(closeFile)
 
 if __name__ == "__main__":
     log(colors.OK,'O2','kdof')
     log(colors.OK,'44OK','ksddof')
-    log(colors.OK,'fs2K','kdodsfff')
+    log(colors.OK,'fs2K','kdodsfff',lambda:time.sleep(1))
     log(colors.OK,'OdK22422347y233','sdfdsfdsfsdfskdof')
     log(colors.OK,'OK333223','kdosfddsfsdfsdfsdfsdkladjeioqwjiurhj2uir ign4tuir 4sdfjisudfsdioj'*9)
     log(colors.OK,'dseesf','sdfsdfdshrttrh4543',lambda:time.sleep(1))
